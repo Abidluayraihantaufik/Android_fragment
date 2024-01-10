79c293f03d56aca22e638d523d12dea600944f6e
Abidluayraihantaufik <spaskeren30@gmail.com>
2024-01-10 08:33:40 +0700

    UAS

--- app/src/main/java/com/example/cakeset/Sinopsis/Sinopsis1.java
+++ app/src/main/java/com/example/cakeset/Sinopsis/Sinopsis1.java
@@ -1,15 +1,23 @@
 package com.example.cakeset.Sinopsis;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity;  // Import AppCompatActivity
 import com.example.cakeset.R;
 
-public class Sinopsis1 extends AppCompatActivity {
+public class Sinopsis1 extends AppCompatActivity {  // Extend AppCompatActivity
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView( R.layout.fragment_sindito );
+        setContentView(R.layout.fragment_sindito);  // Use setContentView to set the layout
+
+        WebView webView1 = findViewById(R.id.webView);
+        String video1 = "<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/DYs0kblXToA\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        webView1.loadData(video1, "text/html", "utf-8");
+        webView1.getSettings().setJavaScriptEnabled(true);
+        webView1.setWebChromeClient(new WebChromeClient());
     }
 }
